a5117d729bab62387d3b2fe2aa6c2435a7df1fc5
AdielSantiago <adielsantiago919@gmail.com>
2026-03-17 00:08:11 -0300

    Create notas.py

---
+++ notas.py
@@ -0,0 +1,42 @@
+from rich import print
+from rich.panel import Panel
+
+class Nota:
+    def __init__(self, nome, *args):
+        #Atributos
+        self.nome = nome
+        self.notas = args
+
+    #métodos
+    def media(self):
+        #Verifica se o argumento args foi passado
+        if not self.notas:
+            return 0
+        
+        #Calcula a média das notas
+        return sum(self.notas) / len(self.notas)
+
+
+    def analisar(self):
+        m = self.media()
+
+        conteudo = f'[u]Nome do aluno: {self.nome}[/u]\n\n'
+
+        for i, nota in enumerate(self.notas, 1):
+            conteudo += f'[green]{i}ºNota[/]: [sky_blue3]{nota}\n[/]'
+
+        if m < 7:
+            conteudo += f'\n[u red]Média final: {m:.1f}[/u red]'
+        else:
+            conteudo += f'\n[u medium_purple1]Média final: {m:.1f}[/u medium_purple1]'
+
+        painel = Panel(conteudo,
+                       title='[blue]Notas do aluno[/]',
+                       border_style='yellow',
+                       expand=False
+                 )
+
+        print(painel)
+
+a1 = Nota('Adiel de freitas santiago', 6, 9, 7)
+a1.analisar()
